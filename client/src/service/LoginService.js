b282563ba047d092af44f3e087521b54ec4d82fa
alperentalu <alperentalu@gmail.com>
2022-02-01 23:00:35 +0300

    Start validation

--- client/src/service/LoginService.js
+++ client/src/service/LoginService.js
@@ -5,7 +5,6 @@ import {gatewayInstance} from "../interceptors/request";
 export const  getLoginReq =  (email, password) => {
      gatewayInstance.post('/login',{password: password, email: email}).then(res => {
         if(res.status === 200){
-            console.log("başarılı")
             localStorage.setItem('token', res.data.token)
             window.location.href = "/"
         }else {
--- client/src/service/RegisterService.js
+++ client/src/service/RegisterService.js
@@ -3,8 +3,12 @@ import axios from "axios"
 export const  getRegisterReq =  (userName, email, password) => {
     const form = JSON.stringify({username: userName, email: email, password: password})
      axios.post('http://localhost:5000/api/register',form,{headers: {'content-type': 'application/json'}}).then(res => {
-       
-        console.log(res)
+       if(res.status === 200) {
+           window.location.href = "/login";
+           return true
+       }else {
+           return false
+       }
 
     })
     console.log(form);
